6904c421606312e24d9385d89841845c1a5f804a
Antti Ketola <antti@anttiketola.com>
2019-05-25 15:00:32 +0300

    Tehtävä 1.14 tehty

--- osa_1/anekdootit/src/index.js
+++ osa_1/anekdootit/src/index.js
@@ -7,13 +7,28 @@ const Button = ({handleClick, text}) => (
   </button> 
 )
 
+const ShowHighest = ({ anecdotes, votes }) => {
+  let highest = 0
+  for (let x = 0; x < 6; x++) {
+    if (votes[highest] < votes[x]) {
+      highest = x
+    }
+  }
+  return (
+    <div>
+      {anecdotes[highest]}<br />
+      Has {votes[highest]} votes.<br />
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0))
 
   const handleRandomClick = () => {
-    const randomAnecote = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomAnecote)
+    const randomAnecdote = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomAnecdote)
   }
 
   const handleVoteClick = () => {
@@ -24,10 +39,13 @@ const App = (props) => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}<br />
       Has {votes[selected]} votes.<br />
       <Button text="Vote" handleClick={handleVoteClick} />
       <Button text="Next anecdote" handleClick={handleRandomClick} />
+      <h1>Anecdote with the most votes</h1>
+      <ShowHighest anecdotes={props.anecdotes} votes={votes} />
     </div>
   )
 }
