04e3a23a16abb22188a4756809e3236167b0768c
Antti Ketola <antti@anttiketola.com>
2019-07-28 14:56:57 +0300

    Tehtävä 2.11 tehty

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterField = ({value, handler }) => {
   return (
@@ -70,6 +71,14 @@ const App = () => {
       setNewNumber('')
     }
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const peopleToShow = filter === '' ? 
     persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
