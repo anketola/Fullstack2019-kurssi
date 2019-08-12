2062c10f3c2e9ce156fa6f936a1838b953cbace0
Antti Ketola <antti@anttiketola.com>
2019-08-12 11:12:31 +0300

    Tehtävä 5.6 tehty

--- osa_5/bloglist-frontend/src/components/Blog.js
+++ osa_5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (visible === false) {
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setVisible(!visible)}>
+        {blog.title} {blog.author}
+      </div>
+    </div>
+  )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setVisible(!visible)}>
+        {blog.title} {blog.author} <br />
+      </div>
+      <div>
+        <a href={blog.url}>{blog.url}</a> <br />
+        {blog.likes} likes
+        <button onClick={() => console.log('pressed like')}>
+          like
+        </button> <br />
+        added by {blog.user.name}
+      </div>
   </div>
-)
+  )
+}
 
 export default Blog
