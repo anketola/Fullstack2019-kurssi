9560ae3cbe0647fa6782d27838522490bc9ff174
Antti Ketola <antti@anttiketola.com>
2019-08-12 13:17:19 +0300

    Tehtävä 5.8 tehty

--- osa_5/bloglist-frontend/src/App.js
+++ osa_5/bloglist-frontend/src/App.js
@@ -177,7 +177,7 @@ function App() {
       />
       </Togglable>
       
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs}/>
       )}
     </div>
