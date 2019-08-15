3a3891497dce3d83032592af89f28c2569395627
Antti Ketola <antti@anttiketola.com>
2019-08-15 13:07:06 +0300

    Tehtävä 6.2 tehty

--- osa_6/unicafe-redux/src/reducer.js
+++ osa_6/unicafe-redux/src/reducer.js
@@ -6,7 +6,7 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
-  console.log(state)
+  // console.log(state)
   switch (action.type) {
     case 'GOOD':
       return { ...state, good : state.good + 1 }
