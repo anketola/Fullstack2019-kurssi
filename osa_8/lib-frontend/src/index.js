977b3d295ba9f8f5a74f27e44589c1a248267d5d
Antti Ketola <antti@anttiketola.com>
2019-11-17 13:27:21 +0200

    Korjattu väärä import

--- osa_8/lib-frontend/src/index.js
+++ osa_8/lib-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from "@apollo/react-hooks"
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
